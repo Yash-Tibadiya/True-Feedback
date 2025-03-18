bc010f875ab71b64ecdaa0406826e5c01c065e36
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-03-18 10:55:37 +0530

    Add API endpoint to send messages with user validation and acceptance check

---
+++ src/app/api/send-messages/route.ts
@@ -0,0 +1,44 @@
+import dbConnect from "@/lib/dbConnect";
+import User from "@/model/User.model";
+
+import { IMessage } from "@/model/User.model";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  const { username, content } = await request.json();
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return Response.json(
+        { success: false, message: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    // is user accepting messages?
+    if (!user.isAcceptingMessages) {
+      return Response.json(
+        { success: false, message: "User is not accepting messages" },
+        { status: 403 }
+      );
+    }
+
+    const newMessage = { content, createdAt: new Date() };
+
+    // Push the new message to the user's messages array
+    user.messages.push(newMessage as unknown as IMessage);
+    await user.save();
+
+    return Response.json(
+      { message: "Message sent successfully", success: true },
+      { status: 201 }
+    );
+  } catch (error) {
+    return Response.json(
+      { success: false, message: "Database error", error },
+      { status: 500 }
+    );
+  }
+}
