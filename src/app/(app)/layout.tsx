8a21f9f699ac35cbb142a160770685da0d9f6432
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-03-19 14:22:45 +0530

    Implement delete message API endpoint and refactor layout component

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,22 +1,5 @@
 import Navbar from "@/components/Navbar";
 import "@/app/globals.css";
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "True Feedback",
-  description: "Where feedback matters",
-};
 
 export default function RootLayout({
   children,
@@ -24,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <div>
       <Navbar />
       {children}
     </div>
