48c4b772f8fa33d55b29c483dcc2e0aa4ef62939
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-03-20 00:48:18 +0530

    Refactor Home component layout to improve background styling and structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <>
-    <Navbar />
-      <div className="flex min-h-screen flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold">Hello 👋</h1>
+      <div className="relative flex flex-col min-h-screen transition-colors duration-300 bg-gradient-to-tr from-blue-400 to-blue-800 dark:bg-black dark:bg-gradient-to-tr dark:from-black dark:to-blue-900 overflow-hidden">
+        <Navbar />
+        <div className="flex min-h-screen flex-col items-center justify-center">
+          <h1 className="text-3xl font-bold">Hello 👋</h1>
+        </div>
       </div>
     </>
   );
